4820975ef417c6be6ef0c20d13c2f56ca6e22efd
ValikWasT <99086125+ValikWasT@users.noreply.github.com>
2022-10-07 12:20:01 +0000

    Deploying to gh-pages from @ ValikWasT/goit-react-hw-05-movies@af2f1c0d2cade1ec7005bda1acfe7acb960f53f1 🚀

---
+++ static/js/201.612a22b7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[201],{201:function(e,t,r){r.r(t);var n=r(731),o=r(184);t.default=function(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h1",{children:"Not Found, 404"}),(0,o.jsx)(n.rU,{to:"/",children:"Go home page"})]})}}}]);
+//# sourceMappingURL=201.612a22b7.chunk.js.map
